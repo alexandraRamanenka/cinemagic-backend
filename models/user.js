3044772df0db348332b269fcd785467fc47946c9
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-10-10 10:22:41 +0300

    Virtual path of User for his reservations

--- models/user.js
+++ models/user.js
@@ -1,22 +1,38 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  login: { type: String, required: [true, "Login is required"], unique: true },
-  email: { type: String, required: [true, "Email is required"] },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    min: 6,
-    max: 24
+const userSchema = new mongoose.Schema(
+  {
+    login: {
+      type: String,
+      required: [true, "Login is required"],
+      unique: true
+    },
+    email: { type: String, required: [true, "Email is required"] },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      min: 6,
+      max: 24
+    },
+    passwordConfirmation: {
+      type: String,
+      required: [true, "Please, confirm your password"]
+    },
+    phone: String,
+    avatar: String,
+    role: { type: String, enum: ["admin", "user"], default: "user" }
   },
-  passwordConfirmation: {
-    type: String,
-    required: [true, "Please, confirm your password"]
-  },
-  phone: String,
-  avatar: String,
-  role: { type: String, enum: ["admin", "user"], default: "user" }
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+userSchema.virtual("reservations", {
+  ref: "Reservation",
+  localField: "_id",
+  foreignField: "user"
 });
 
 userSchema.path("passwordConfirmation").validate(function(value) {
