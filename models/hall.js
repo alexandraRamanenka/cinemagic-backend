9f4d9020ec376ceb2605eefac16e8ebe31cd2576
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-10-10 10:32:26 +0300

    Counting number of seats in halls

--- models/hall.js
+++ models/hall.js
@@ -1,16 +1,28 @@
 const { Schema, model } = require("mongoose");
 const lineSchema = require("./line");
 
-const hallSchema = new Schema({
-  cinema: {
-    type: Schema.Types.ObjectId,
-    ref: "Cinema",
-    required: [true, "Cinema is required"]
+const hallSchema = new Schema(
+  {
+    cinema: {
+      type: Schema.Types.ObjectId,
+      ref: "Cinema",
+      required: [true, "Cinema is required"]
+    },
+    seatsSchema: {
+      type: [lineSchema],
+      required: [true, "Seats schema is required"]
+    }
   },
-  seatsSchema: {
-    type: [lineSchema],
-    required: [true, "Seats schema is required"]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-});
+);
 
+hallSchema.virtual("seatsNumber").get(function() {
+  let seats = this.seatsSchema.reduce((acc, line) => {
+    return (acc += line.numberOfSeats);
+  }, 0);
+  return seats;
+});
 module.exports = new model("Hall", hallSchema);
