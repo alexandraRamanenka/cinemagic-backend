e5b62d498e38a3b0493db420c8ceea67cf0ea4fe
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-10-16 14:38:06 +0300

    CORS middlware added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const AppError = require('./utiles/appError');
 const globalErrorController = require('./controllers/globalErrorController.js');
 const routes = require('./routes');
 
 //Parsing middleware
+app.use(cors());
 app.use(express.json());
 
 app.use('/', routes);
