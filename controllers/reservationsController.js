bd7a9f45061226a1b47797b0630c9cb9459ec3de
butterflyInACast <butterfly.in.a.cast@gmail.com>
2019-11-12 10:47:22 +0300

    Populating reservations with service price

--- controllers/reservationsController.js
+++ controllers/reservationsController.js
@@ -1,22 +1,22 @@
-const Reservation = require('../models/Reservation');
-const AppError = require('../utiles/appError');
-const catchAsync = require('../utiles/catchAsync');
-const handlersFactory = require('./handlersFactory');
+const Reservation = require("../models/Reservation");
+const AppError = require("../utiles/appError");
+const catchAsync = require("../utiles/catchAsync");
+const handlersFactory = require("./handlersFactory");
 
 const validateSeats = catchAsync(async (req, res, next) => {
   let seats, session;
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     seats = req.body.seats;
     session = req.body.session;
   }
-  if (req.method === 'PATCH') {
+  if (req.method === "PATCH") {
     const reservation = await Reservation.findById(req.params.reservationId);
     seats = req.body.seats || reservation.seats;
     session = req.body.session || reservation.session;
   }
 
   if (!(await checkAllSeats(session, seats, req.params.reservationId))) {
-    return next(new AppError('Seat is already reserved', 400));
+    return next(new AppError("Seat is already reserved", 400));
   }
   next();
 });
@@ -53,13 +53,13 @@ module.exports.validateSeats = validateSeats;
 module.exports.findAllReservations = handlersFactory.getAll(Reservation);
 module.exports.getReservationById = handlersFactory.getOne(
   Reservation,
-  'reservationId'
+  "reservationId"
 );
 module.exports.createReservation = handlersFactory.createOne(Reservation);
 module.exports.updateReservation = catchAsync(async (req, res, next) => {
   let reservation = await Reservation.findById(req.params.reservationId);
   if (!reservation) {
-    return next(new AppError('Reservation is not found', 404));
+    return next(new AppError("Reservation is not found", 404));
   }
 
   reservation.seats = req.body.seats ? req.body.seats : reservation.seats;
@@ -70,28 +70,28 @@ module.exports.updateReservation = catchAsync(async (req, res, next) => {
   await reservation.save();
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     reservation
   });
 });
 
 module.exports.deleteReservation = handlersFactory.deleteOne(
   Reservation,
-  'reservationId'
+  "reservationId"
 );
 
 module.exports.getReservationsForCurrentUser = catchAsync(
   async (req, res, next) => {
     if (!req.user) {
-      return next(new AppError('Please, log in or sign up', 401));
+      return next(new AppError("Please, log in or sign up", 401));
     }
 
     const reservations = await Reservation.find({ user: req.user.id })
-      .populate({ path: 'session', select: ['film', 'dateTime'] })
-      .populate({ path: 'services.service', select: ['name'] });
+      .populate({ path: "session", select: ["film", "dateTime"] })
+      .populate({ path: "services.service", select: ["name", "price"] });
 
     res.status(200).json({
-      status: 'succcess',
+      status: "succcess",
       data: reservations
     });
   }
