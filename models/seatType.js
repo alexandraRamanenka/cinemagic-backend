cb81238ece2a26ef9ec125239e426cee8206e291
butterflyInACast <butterfly.in.a.cast@gmail.com>
2020-01-10 18:48:57 +0300

    Some fixes

--- models/seatType.js
+++ models/seatType.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const seatTypeSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Name is required'] },
   description: { type: String },
-  price: { type: Number, required: [true, 'Price is required'] }
+  price: { type: Number, required: [true, 'Price is required'] },
+  view: {
+    type: {
+      width: Number,
+      transform: String,
+      icon: String,
+      text: String
+    }
+  }
 });
 
 const SeatType = new mongoose.model('SeatType', seatTypeSchema);
